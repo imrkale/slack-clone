f9401030895f1c97c875cb03f7e4dc8b098ab6b5
Raj Kale <kale44772@gmail.com>
2021-02-22 00:06:49 +0530

    Header Added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import './App.css';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Header from './components/Header'
+import styled from 'styled-components';
+import SideBar from './components/SideBar'
 function App() {
   return (
     <div className="App">
-     <h1>Welcome to Slack Clone</h1>
+    <Router>
+     
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Header/>
+        <AppBody>
+          <SideBar/>
+          <Switch>
+              <Route path="/">
+
+              </Route>
+          </Switch>
+        </AppBody>
+      
+    </Router>
     </div>
   );
 }
-
 export default App;
+
+const AppBody=styled.div``;
---
+++ src/components/Header.js
@@ -0,0 +1,83 @@
+import React from 'react'
+import styled from 'styled-components'
+import {Avatar} from '@material-ui/core'
+import AccessTimeIcon from '@material-ui/icons/AccessTime'
+import SearchIcon from '@material-ui/icons/Search'
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline'
+function Header() {
+    return (
+        <HeaderContainer>
+           {/* Header Left */}
+           <HeaderLeft>
+               <HeaderAvatar/>
+               <AccessTimeIcon/>
+           </HeaderLeft>
+           <HeaderSearch>
+                <SearchIcon/>
+                <input placeholder="Search in chat"/>
+           </HeaderSearch>
+           <HeaderRight>
+                <HelpOutlineIcon/>
+           </HeaderRight>
+           {/* Header Search */}
+           {/* Header Right */}
+        </HeaderContainer>
+    )
+}
+
+export default Header
+
+const HeaderContainer= styled.div`
+    display:flex;
+    position:fixed;
+    width:100%;
+    align-items:center;
+    justify-content:space-between;
+    padding:10px 0;
+    background-color:var(--slack-color);
+    color:white;
+`;
+const HeaderLeft=styled.div`
+    flex:0.3;
+    display:flex;
+    align-items:center;
+    margin-left:20px;
+    
+    > .MuiSvgIcon-root {
+        margin-left:auto;
+        margin-right:30px;
+    }
+`;
+const HeaderAvatar=styled(Avatar)`
+    cursor: pointer;
+    :hover{
+        opacity:0.8;
+    }
+`;
+const HeaderSearch=styled.div`
+    flex:0.4;
+    display:flex;
+    align-items:center;
+    padding:5px 50px;
+    opacity:1.4;
+    background-color:#421f44;
+    border-radius:6px;
+    /* border:2px grey solid; */
+    >input{
+        border:none;
+        color:white;
+        background-color:transparent;
+        text-align:center;
+        min-width:30px;
+        outline:none;
+    }
+`;
+const HeaderRight=styled.div`
+    display:flex;
+    flex:0.3;
+    align-items:flex-end;
+    >.MuiSvgIcon-root{
+        margin-left:auto;
+        margin-right:20px;
+    }
+`;
